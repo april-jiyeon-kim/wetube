f87ae6d24b369489c1543ab0f3758364050bb13b
april-jiyeon-kim <jiyeon95417@gmail.com>
2019-09-07 21:05:29 +0900

    feat: Add editVideo page
    Get video to editVideo.pug file using video id and update the video after editing using findOneAndUpdate of mongoose

--- routes.js
+++ routes.js
@@ -34,7 +34,13 @@ const routes = {
           return USER_DETAIL;
         }
       },
-    editProfile: EDIT_PROFILE,
+    editProfile: id => {
+      if (id) {
+        return `/videos/${id}/edit`;
+      }else{
+        return EDIT_PROFILE
+      }
+    },
     changePassword: CHANGE_PASSWORD,
     videos: VIDEOS,
     upload: UPLOAD,
@@ -45,7 +51,14 @@ const routes = {
             return VIDEO_DETAIL
         }
     },
-    editVideo: EDIT_VIDEO,
+    editVideo: id => {
+      if (id) {
+        return `/videos/${id}/edit`;
+      } else {
+        return EDIT_VIDEO;
+      }
+    },
+
     deleteVideo: DELETE_VIDEO
 };
 
