e639b14f25b74c54320c842f81b893530c1ab20c
april-jiyeon-kim <jiyeon95417@gmail.com>
2019-09-07 21:18:00 +0900

    feat: Add deleteVideo page
    Get video using video id and delete the video using findOneAndRemove of mongoose, then redirect to home page

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,7 +41,7 @@ export const videoDetail = async (req,res) => {
     } = req;
     try{
         const video = await Video.findById(id);
-        res.render("videoDetail", { pageTitle: "Video Detail" ,video});
+        res.render("videoDetail", { pageTitle: video.title ,video});
     }catch{
         console.log(error);
         res.redirect(routes.home);
@@ -51,7 +51,7 @@ export const videoDetail = async (req,res) => {
 export const getEditVideo = async (req, res) => {
     const {
       params: { id }
-    } = req;
+    } = req; 
     try {
       const video = await Video.findById(id);
       res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
@@ -72,4 +72,14 @@ export const getEditVideo = async (req, res) => {
       res.redirect(routes.home);
     }
   };
-export const deleteVideo = (req,res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+
+export const deleteVideo = async(req,res) => {
+    const {
+        params:{id}
+    }=req;
+    try{
+        await Video.findOneAndRemove({_id:id});
+    } catch(error){
+    }
+    res.redirect(routes.home);
+};
