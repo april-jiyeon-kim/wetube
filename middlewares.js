3bba887fb554f168b4302443f12f23b67093705c
april-jiyeon-kim <yeon19april@gmail.com>
2019-09-09 15:14:17 +0900

    feat: Make the footer and the login/join using SCSS

--- middlewares.js
+++ middlewares.js
@@ -1,17 +1,16 @@
-import multer from "multer";   
+import multer from "multer";
 import routes from "./routes";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
- 
 
-export const localsMiddleware = (req,res,next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAutenticated: true,
-        id: 1
-    };
-    next();
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAutenticated: false,
+    id: 1
+  };
+  next();
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
